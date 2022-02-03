e2d16604977a0e638e0bdd2ea1894af8bd9f8694
CondeArcana333 <98907999+CondeArcana333@users.noreply.github.com>
2022-02-03 11:54:16 -0600

    mando a rrp. local

---
+++ intento_v_1.py
@@ -0,0 +1,17 @@
+#promedio y desviacion estandar
+a=input("dame una lista de numero:\n")
+lista=a.split(",")
+
+def promedio_std(lista):
+    suma = 0
+    for elemento in lista:
+        suma += int(elemento) 
+        promedio=suma/len(lista)
+    from math import sqrt
+    sigma=0
+    for elemento in lista:
+        sigma += (float(elemento)-promedio)**2
+        desv=sqrt(sigma/len(lista))
+    return ("el promedio de los numeros dados es:",promedio," la desviacion estandar de los numeros dados es:",desv)
+print(promedio_std(lista))
+input("")
